4a30a9f261fedaf14411909adc1cb7d2e31dbc4f
fabiocaiulo8 <fabiocaiulo8@gmail.com>
2022-04-05 18:00:00 +0200

    Creation of the Angular workspace

---
+++ src/app/typings.d.ts
@@ -0,0 +1,6 @@
+declare module '*.json';
+declare module 'openseadragon';
+declare module '@recogito/annotorious-openseadragon';
+declare module '@recogito/annotorious-selector-pack';
+declare module '@recogito/annotorious-better-polygon';
+declare module '@recogito/annotorious-toolbar';
